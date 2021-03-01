f898ddacd9f2371866b2fa12c577be9bdcd504fd
codingSJ <seojoon.hong94@gmail.com>
2021-03-01 21:41:09 +0900

    .

---
+++ 20944.cpp
@@ -0,0 +1,18 @@
+// base setting
+#include <iostream>
+
+using namespace std;
+using ll = long long;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int N;
+    cin >> N;
+
+    while(N--){
+        cout << "a";
+    }
+}
